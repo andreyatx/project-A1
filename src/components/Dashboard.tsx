372591a9225d861460086db37b89ea3fdfbb3355
andreyatx <mitrofanobandrei@gmail.com>
2023-03-01 18:14:17 +0300

    feat(firebase): add cloud storage

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,6 @@
+import { push, ref, set } from 'firebase/database';
 import { type FC } from 'react';
+import { db } from '../firebase';
 
 import { categorySelectors } from '../store/features/category/categorySlice';
 import { useAppSelector } from '../store/hooks';
@@ -6,6 +8,13 @@ import { Category } from './Category';
 
 export const Dashboard: FC = () => {
 	const { categories } = useAppSelector(categorySelectors.all);
+	// const catListRef = ref(db, 'categories');
+	// const newCatRef = push(catListRef);
+
+	// set(newCatRef, {
+	// 	title: 'Backlog',
+	// 	id: 1,
+	// });
 
 	return (
 		<div className="flex flex-row space-x-8">
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +22,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const db = getDatabase(app);
 
-export { db };
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage(app);
+
+export { db, storage };
