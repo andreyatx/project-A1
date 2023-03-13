d1b6dda94746c3bae530bc26702ab5eff47d2f11
andreyatx <mitrofanobandrei@gmail.com>
2023-03-13 20:56:05 +0300

    fix: dropdown

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -29,45 +29,56 @@ export const Task: FC<TaskProps> = ({ task, index }) => {
 
 	return (
 		<Draggable draggableId={task.taskId} index={index}>
-			{provided => (
-				<div
-					className="card w-80 h-24 bg-neutral rounded-sm flex flex-row p-2 mb-2"
-					{...provided.draggableProps}
-					{...provided.dragHandleProps}
-					ref={provided.innerRef}>
-					<div className="left-block flex flex-col">
-						<div className="mb-2">{task.title ?? 'Название задачи'}</div>
+			{provided => {
+				return (
+					<div
+						className="card w-80 h-24 bg-neutral rounded-sm flex flex-row p-2 mb-2"
+						{...provided.dragHandleProps}
+						{...provided.draggableProps}
+						ref={provided.innerRef}>
+						<div className="left-block flex flex-col">
+							<div className="mb-2">{task.title ?? 'Название задачи'}</div>
 
-						<div className="flex flex-row mt-auto">
-							<div className="text-sm text-slate-300">Приоритет {task.priority ?? 'Priority'}</div>
-							<div className="text-sm text-slate-300 ml-2">{task.tags ?? 'Tags'}</div>
+							<div className="flex flex-row mt-auto">
+								<div className="text-sm text-slate-300">Приоритет {task.priority ?? 'Priority'}</div>
+								<div className="text-sm text-slate-300 ml-2">{task.tags ?? 'Tags'}</div>
+							</div>
+						</div>
+
+						<div className="right-block ml-auto flex flex-col justify-between">
+							<Avatar avatar={task.avatar} />
+
+							<div
+								// Set data-attributes to this div to disable drag and allow dropdown to work
+								data-rbd-drag-handle-context-id={provided.dragHandleProps?.['data-rbd-drag-handle-context-id']}
+								data-rbd-drag-handle-draggable-id="gibberish"
+								style={{
+									cursor: 'auto',
+								}}
+								className="flex dropdown dropdown-end">
+								<button
+									onClick={() => console.log('123')}
+									className="max-w-fit bg-transparent rounded-md focus:bg-slate-400 hover:bg-slate-400 ml-2">
+									<EllipsisHorizontalIcon className="h-6 w-6 text-white self-center" />
+								</button>
+								<ul className="dropdown-content menu shadow bg-neutral rounded-box mt-6">
+									<li>
+										<button className="hover:bg-slate-400">Редактировать</button>
+									</li>
+									<li>
+										<button
+											draggable={false}
+											onClick={() => deleteHandler(task.categoryId, task)}
+											className="hover:bg-slate-400 text-red-600">
+											Удалить
+										</button>
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
-
-					<div className="right-block ml-auto flex flex-col justify-between">
-						<Avatar avatar={task.avatar} />
-
-						<div className="flex dropdown dropdown-end">
-							<button className="max-w-fit bg-transparent rounded-md focus:bg-slate-400 hover:bg-slate-400 ml-2">
-								<EllipsisHorizontalIcon className="h-6 w-6 text-white self-center" />
-							</button>
-							<ul className="dropdown-content menu shadow bg-neutral rounded-box mt-6">
-								<li>
-									<button className="hover:bg-slate-400">Редактировать</button>
-								</li>
-								<li>
-									<button
-										draggable={false}
-										onClick={() => deleteHandler(task.categoryId, task)}
-										className="hover:bg-slate-400 text-red-600">
-										Удалить
-									</button>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			)}
+				);
+			}}
 		</Draggable>
 	);
 };
